ad7435e1c0e7923f9b26a49f50816b27157487d8
Cem-Kaplan <cemk46908@gmail.com>
2025-06-17 17:19:10 +0200

    pages hinzugefügt

--- uploadplattform/hauptseite/views.py
+++ uploadplattform/hauptseite/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def show_home(request):
     return render(request, 'hauptseite/index.html')
+
+def show_about(request):
+    return render(request, 'hauptseite/about.html')
+    
+def show_upload(request):
+    return render(request, 'hauptseite/upload.html')
+    
+def show_login(request):
+    return render(request, 'hauptseite/login.html')
