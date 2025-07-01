b695a424b7701a2548ef80308536a2d57fd3d1e2
Cem-Kaplan <cemk46908@gmail.com>
2025-07-01 10:40:12 +0200

    suchfeld hinzugefügt

--- uploadplattform/videos/urls.py
+++ uploadplattform/videos/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.for_you_page, name='for_you'),
     path('video/<int:video_id>/', views.video_detail, name='video_detail'),
-    path('upload', views.upload, name='upload'),
+    path('upload/', views.upload, name='upload'),
 ]
--- uploadplattform/videos/views.py
+++ uploadplattform/videos/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Video
+from django.db.models import Q
 
 def for_you_page(request):
-    videos = Video.objects.all().order_by('-uploaded_at')
+    query = request.GET.get('q')
+    if query:
+        videos = Video.objects.filter(
+            Q(title__icontains=query) | Q(description__icontains=query)
+        ).order_by('-uploaded_at')
+    else:
+        videos = Video.objects.all().order_by('-uploaded_at')
+    
     return render(request, 'videos/for_you.html', {'videos': videos})
 
 def video_detail(request, video_id):
